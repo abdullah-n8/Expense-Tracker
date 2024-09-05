939e16f4d33271298dd6f91053de25c8a3fbc19b
Abdullah Naeem <raoabdullahn@gmail.com>
2024-09-05 13:18:58 +0500

    Integrated the UI and made them into simpler components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+// custom Components
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+    <div className="container">
+      <Balance />
+      <IncomeExpense />
+      <TransactionList />
+      <AddTransaction />
+    </div>
     </div>
   );
 }
